97d350d0be3427d430183206f408882833a26880
omer ozkul <omer.ozkul@epitech.eu>
2014-11-28 20:51:17 +0100

    add Rss.cs file

---
+++ mobilApp/FeedReader/Model/Rss.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using System.ServiceModel.Syndication;
+
+namespace FeedReader.Model
+{
+    public class RSS
+    {
+        public int      Id { get; set; }
+
+        public string   URL { get; set; }
+
+        public string   Flux { get; set; }
+
+        public int      CategoryId { get; set; }
+        
+        public int   UserId { get; set; }
+
+        public BitmapImage image { get; set; }
+
+        public int numberOfFeed { get; set; }
+
+        public SyndicationFeed newsListe { get; set; }
+
+    }
+}
